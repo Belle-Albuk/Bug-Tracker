598901d9caca8a1f89c1230dc93eba817872328b
Isabelle Albuquerque <bellealbuk@gmail.com>
2023-01-24 10:58:38 -0300

    fix: .gitignore

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.route('/')
     .get(function (req, res) {
         res.sendFile(process.cwd() + '/views/index.html')
+    });
+
+// Log in page
+app.route('/login')
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/login.html')
     })
 
+// Sign up page
+app.route('/signup')
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/signup.html')
+    })
+    
 const listener = app.listen(3000, function () {
     console.log('Your app is listening on port ' + listener.address().port)
 });
