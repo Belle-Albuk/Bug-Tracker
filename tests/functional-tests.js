e5cefc73bcb51a0762b68241ee2c23e5665694cc
Isabelle Albuquerque <bellealbuk@gmail.com>
2023-02-19 14:00:48 -0300

    test: PUT and DELETE method on /profile/api/:id

--- tests/functional-tests.js
+++ tests/functional-tests.js
@@ -10,77 +10,93 @@ let issueId;
 
 suite('Functional Tests', function () {
 
-    test('homepage should be working', function(done) {
-      chai
-        .request(server)
-        .get('/')
-        .end(function (err, res) {
-            assert.equal(res.status, 200);
-            done();
-        })
+    test('homepage should be working', function (done) {
+        chai
+            .request(server)
+            .get('/')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                done();
+            })
     });
 
-    test('logging in with invalid user should redirect to the homepage', function(done) {
+    test('logging in with invalid user should redirect to the homepage', function (done) {
         chai
             .request(server)
             .post('/account/login')
             .type('form')
-            .send({username: 'InvalidUser', password: 'dontExist'})
-            .end(function(err, res) {
+            .send({ username: 'InvalidUser', password: 'dontExist' })
+            .end(function (err, res) {
                 res.should.redirectTo(res.request.url);
                 done();
             })
     })
 
-    test('logging in with a valid user should redirect to /profile', function(done) {
+    test('logging in with a valid user should redirect to /profile', function (done) {
         chai
             .request(server)
             .post('/account/login')
             .type('form')
-            .send({username: 'userTest', password: '123'})
-            .end(function(err, res) {
+            .send({ username: 'userTest', password: '123' })
+            .end(function (err, res) {
                 res.should.redirectTo(res.request.url + 'profile');
                 done();
             })
     })
 
-    test('GET method on /profile/api should return an array', function() {
+    test('testing GET and POST method for /profile/api, PUT and DELETE method for /profile/api/:id', function () {
         agent
             .post('/account/login')
             .type('form')
-            .send({username: 'userTest', password: '123'})
-            .then(async function(res) {
-                await agent.get('/profile/api')
-                            .then(function(res) {
-                                assert.isArray(res.body, 'res.body should be an array');
-                                
-                            }).catch((err) => console.log(err))
-            }).catch((err) => console.log(err))
-    });
+            .send({ username: 'userTest', password: '123' })
+            .then(async function (res) {
 
-    test('POST method on /profile/api should return an object that contains the data that was sent', function() {
-        agent
-            .post('/account/login')
-            .type('form')
-            .send({username: 'userTest', password: '123'})
-            .then(async function(res) {
+                await agent.get('/profile/api')
+                    .then(function (res) {
+                        assert.isArray(res.body, 'res.body should be an array');
+
+                    }).catch((err) => console.log(err))
+
+            })
+            .then(async function (res) {
+
                 await agent.post('/profile/api')
-                            .send({
-                                title: 'testing-Title',
-                                description: 'testing text',
-                                assigned_to: 'testingUser',
-                                priority: 'low'})
-                            .then(function(res) {
-                                issueId = res.body._id;
-                                assert.include(res.body, {
-                                created_by: 'userTest',
-                                bug_title: 'testing-Title',
-                                bug_description: 'testing text',
-                                assigned_to: 'testingUser',
-                                priority: 'low',
-                                open: true}, 'res.body contains the sent properties + open is true');
-                                agent.close();                                
-                            }).catch((err) => console.log(err))
-            }).catch((err) => console.log(err))
+                    .send({
+                        title: 'testing-Title',
+                        description: 'testing text',
+                        assigned_to: 'testingUser',
+                        priority: 'low'
+                    })
+                    .then(function (res) {
+                        issueId = res.body._id;
+                        console.log('id_: ', issueId);
+                        assert.include(res.body, {
+                            created_by: 'userTest',
+                            bug_title: 'testing-Title',
+                            bug_description: 'testing text',
+                            assigned_to: 'testingUser',
+                            priority: 'low',
+                            open: true
+                        }, 'res.body contains the sent properties + open is true');
+                    }).catch((err) => console.log(err))
+
+            })
+            .then(async function (res) {
+                await agent.put('/profile/api/' + issueId)
+                    .send({ close: false })
+                    .then(function (res) {
+                        assert.equal(res.body.result, 'successfully updated');
+                        assert.equal(res.body._id, issueId);
+                    }).catch((err) => console.log(err))
+            })
+            .then(async function (res) {
+                await agent.delete('/profile/api/' + issueId)
+                    .then(function (res) {
+                        assert.equal(res.body.result, 'successfully deleted');
+                        assert.equal(res.body._id, issueId);
+                        agent.close();
+                    }).catch((err) => console.log(err))
+            })
+            .catch((err) => console.log(err))
     })
 });
